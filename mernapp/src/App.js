c23d9cdc9d555201a2b610d8cc4c42209d38c59d
devannshi-24 <devannshii24@gmail.com>
2025-10-25 11:58:54 +0530

    Initial Commit

--- mernapp/src/App.js
+++ mernapp/src/App.js
@@ -1,12 +1,26 @@
 import "./App.css";
 import Home from "./screens/Home";
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Login from "./screens/Login";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-night.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function App() {
   return (
-    <>
-      <div><Home/></div>
-    </>
+    <Router>
+      <div data-bs-theme="dark">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+        </Routes>
+
+      </div>
+    </Router>
   );
 }
 
